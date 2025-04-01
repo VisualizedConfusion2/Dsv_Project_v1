41f1ee1110d28e98839081dbbba17ca72fc5f08b
Carina43 <cag003@edu.zealand.dk>
2025-04-01 13:50:11 +0200

    udkommenteret rooms

--- Dsv_Project_v1/Pages/Rooms.cshtml.cs
+++ Dsv_Project_v1/Pages/Rooms.cshtml.cs
@@ -7,16 +7,11 @@
 //{
 //    public class RoomsModel : PageModel
 //    {
-//        public List<MeetingRoom> MeetingRooms { get; set; } = new();
+//        //public List<MeetingRoom> MeetingRoom { get; set; } = new();
 //        public void OnGet()
 //        {
 
-//            MeetingRooms = new List<MeetingRoom>
-//            {
-//                new MeetingRoom {  Name = "Mødelokale A", Capacity = 10, Equipment = "Projektor, Whiteboard", IsAvailable = true, Id=1 },
-//                new MeetingRoom {  Name = "Mødelokale B", Capacity = 8, Equipment = "TV, Konferenceudstyr", IsAvailable = true, Id=2 },
-//                new MeetingRoom {  Name = "Mødelokale C", Capacity = 12, Equipment = "Whiteboard", IsAvailable = true, Id=3 }
-//            };
+            
 //        }
 //    }
 //}
