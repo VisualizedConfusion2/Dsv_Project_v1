46d821336dbeb567b5f03b8601a98c87312498f1
VisualizedConfusion2 <thh005.edu.zealand.dk>
2025-04-02 09:11:14 +0200

    Added mine bokings

---
+++ Dsv_Project_v1/Pages/PersonalBookings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dsv_Project_v1.Pages
+{
+    public class PersonalBookingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
